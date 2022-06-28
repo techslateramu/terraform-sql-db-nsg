77fe6e4b8cca2b4973da727f607c72e1af5d3c10
koti1576 <93080621+koti1576@users.noreply.github.com>
2022-06-28 23:34:33 +0100

    added mysql flexserver module

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # create test resource group
 resource "azurerm_resource_group" "test" {
-  name     = "test-rg"
+  name     = "test-rg123"
   location = "East US"
 }
 
-module "mysql_singleserver" {
-  source = "./modules/mysqlserver"
+module "sqlserver_flex" {
+  source = "./modules/mysqlflexserver"
   rg_name = azurerm_resource_group.test.name
   rg_loc = azurerm_resource_group.test.location
 }
