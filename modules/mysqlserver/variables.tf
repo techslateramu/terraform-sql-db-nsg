928e9ac0f8fe26684a631f4b57b729aef15cc29f
koti1576 <93080621+koti1576@users.noreply.github.com>
2022-06-30 11:30:09 +0100

    Update variables.tf

--- modules/mysqlserver/variables.tf
+++ modules/mysqlserver/variables.tf
@@ -33,19 +33,19 @@ variable "sql_version" {
     default     = "5.7"
 }
 
-variable "public_network_access" {
+variable "public_network_access_enabled" {
     description = "Whether or not public network access is allowed for this server"
     type        = bool
-    default     = "true"
+    default     = true
 }
 
-variable "ssl_enforcement" {
+variable "ssl_enforcement_enabled" {
     description = "Specifies if SSL should be enforced on connections"
     type        = bool
-    default     = "true"
+    default     = true
 }
 
-variable "force_ssl_minimal_version" {
+variable "ssl_minimal_tls_version_enforced" {
   description = "The minimal SSL version enforced"
   type        = string
   default     = "TLS1_2"
@@ -53,6 +53,6 @@ variable "force_ssl_minimal_version" {
 
 variable "sku_name" {
     description = "Specifies the SKU Name for this MySQL Server"
-    type        = "string"
+    type        = string
     default     = "B_Gen5_2"
 }
